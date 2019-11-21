156b36e7f514ffb2fa18fdda5978d1f7613b4549
Farhad Ahmed <fahmed@wa-fahmed-mbp.local>
2019-11-20 20:26:55 -0800

    added a controller and stored content on server

--- server.js
+++ server.js
@@ -4,6 +4,32 @@ let port = process.env.PORT || 3000;
 
 app.use(express.static(__dirname + '/app/'));
 
+let movies = [
+	{
+		title: 'The Terminator',
+		rating: 8.0,
+		releaseYear: 1984,
+		length: 120
+	},
+	{
+		title: 'Iron Man',
+		rating: 7.9,
+		releaseYear: 2008,
+		length: 118
+	},
+	{
+		title: 'Star Wars: Return of the Jedi',
+		rating: 8.3,
+		releaseYear: 1983,
+		length: 102
+	}
+]
+
+app.get('/movies', function(request, response) {
+	console.log('Server received GET request from Angular controller');
+	console.log(movies);
+});
+
 app.listen(port, function() {
     console.log('server starting at http://localhost:' + port);
 });
